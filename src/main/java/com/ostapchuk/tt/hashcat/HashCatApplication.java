ab28bf2cb499f670a463d4828cff0523beb89b24
k.ostapchuk <xfaseex1@gmail.com>
2022-01-02 22:50:21 +0300

    feat: add profiles

--- src/main/java/com/ostapchuk/tt/hashcat/HashCatApplication.java
+++ src/main/java/com/ostapchuk/tt/hashcat/HashCatApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // TODO: 12/26/2021 add Liquibase
-// TODO: 12/26/2021 add Profiles
 // TODO: 12/26/2021 add Tests
 // TODO: 12/26/2021 add Swagger
 // TODO: 12/29/2021 check handling of Exception
