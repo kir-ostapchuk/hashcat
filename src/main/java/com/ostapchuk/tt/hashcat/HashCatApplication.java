8541378bdbe168c5c4f2fb10de5cbf0010694427
k.ostapchuk <xfaseex1@gmail.com>
2022-01-04 10:12:07 +0300

    feat: add open api

--- src/main/java/com/ostapchuk/tt/hashcat/HashCatApplication.java
+++ src/main/java/com/ostapchuk/tt/hashcat/HashCatApplication.java
@@ -3,11 +3,10 @@ package com.ostapchuk.tt.hashcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO: 12/26/2021 add Tests(IT, AT, test containers)
-// TODO: 12/26/2021 add Swagger
 // TODO: 12/29/2021 check handling of Exception
 // TODO: 12/29/2021 check transactional is it really needed
 // TODO: 1/3/2022 rework services
+// TODO: 12/26/2021 add Tests(IT, AT, test containers)
 
 @SpringBootApplication
 public class HashCatApplication {
